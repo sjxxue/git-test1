ddd4ca12c5d23698980c5aff786f5aa47a76a833
itcast <3056932853@qq.com>
2022-10-03 13:30:18 +0800

    one commit

---
+++ src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,test~1");
+        System.out.println("hello,test~2");
+        System.out.println("hello,test~3");
+        System.out.println("hello,test~4");
+    }
+}
